5601af35cd5f8ad90a3a5f709f627373dfb5514e
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-08-22 20:20:21 -0300

    Criando os diretorios de components

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import HeaderContact from '../components/headerContact/headerContact';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <HeaderContact/>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/headerContact/headerContact.jsx
@@ -0,0 +1,15 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './headerContact.css'
+
+export default function HeaderContact(){
+    return(
+        <div className='headerContact'>
+            <div className='logo-name'>
+                <img src="./logo_uaizap.png" alt="Logo da aplicação UaiZap" />
+                <h1>UaiZap</h1>
+            </div>
+            <FontAwesomeIcon className='icon' icon={faBars} />
+        </div>
+    )
+}
